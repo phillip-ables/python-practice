021a800583919255ccac3c1f2bea09819e1ea55a
Phillip <phillipables1@gmail.com>
2018-02-24 14:12:38 -0500

    for statement to append elements greater than five then input variable and another for element greater than input remove from origianl array

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
   # http://www.practicepython.org/exercise/2014/02/15/03-list-less-than-ten.html
 uIn = [int(x) for x in input("Please enter a list of numbers: ").split()]
+mOut = [];
+for element in uIn:
+    if(element < 5):
+        mOut.append(element)
+print("Numbers below five:")
+print(mOut)
+singleDigit = int(input("Please enter a single digit: "))
+for element in uIn:
+    if(element > singleDigit):
+        uIn.remove(element)
+print("This are inputs smaller than the number you gave: ")
 print(uIn)
